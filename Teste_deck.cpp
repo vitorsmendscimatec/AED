45fd606107321cdf58525b6a080e5485ae58df52
GabrielCalmon <jgabrielcalmon1@hotmail.com>
2020-12-02 11:19:33 -0300

    Teste da função ataque()

--- Teste_deck.cpp
+++ Teste_deck.cpp
@@ -1,23 +1,30 @@
 #include "includes.h"
 
 int main(){
-    Mao *m;
+    Mao *mA, *mB;
    
     POKEMON *squirtle, *bulbasaur;
     PILHA *p;
 
     p = cria_pilha();
-	m = criarMao();
-
+	mA = criarMao();
+	mB = criarMao();
+	
     squirtle = cria_pokemon("Squirtle", "Water", 30, 8 );
     bulbasaur = cria_pokemon("Bulbasaur", "Grass", 30, 8 );
 
-    inserirOrdenadoMao(m, squirtle);
-    inserirOrdenadoMao(m, bulbasaur);
-    imprimirMao(m);
-
+    inserirOrdenadoMao(mA, squirtle);
+    inserirOrdenadoMao(mB, bulbasaur);
+    imprimirMao(mA);
+	imprimirMao(mB);
+	
     printf("========================\n");
-    atacar(squirtle, squirtle);
+    atacar(&mA->pokemon[0], &mB->pokemon[0]);
+    
+    printf("Mao A\n");
+    imprimirMaoHorizontal(mA);
+    printf("Mao B\n");
+    imprimirMaoHorizontal(mB);
 
     
     return 0;
