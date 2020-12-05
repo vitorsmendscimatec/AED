b154f4a59412b0cce371820e660228369b307356
GabrielCalmon <jgabrielcalmon1@hotmail.com>
2020-12-04 21:00:36 -0300

    Atualização da pokedex
    Atualização do hp e atk dos pokemons

--- Teste_arq.cpp
+++ Teste_arq.cpp
@@ -5,8 +5,7 @@
 
 int main(){
     POKEMON *Biancossauro, *Meowth, *lapossauro,* squirtle, *bulbasaur, *Charmander, *Pidgey, *Rattata, *Vulpix, *Poliwag, *Pikachu;
-	Biancossauro = cria_pokemon((char*)"Biancossauro", (char*)"Fire", 40, 5);
-	Meowth = cria_pokemon((char*)"Meowth", (char*)"Normal", 25, 10);
+
 	
     //Modelo sobre como criar um pokemon
     lapossauro = cria_pokemon("Lapossauro", "Normal", 100, 50);
@@ -20,6 +19,18 @@ int main(){
     Poliwag = cria_pokemon("Poliwag", "Water", 35, 5 );
 	Pikachu = cria_pokemon("Pikachu", "Normal", 35, 5);
 	//Abra = cria_pokemon("Abra", "Physhic", 30, 7);
+	
+	Biancossauro = cria_pokemon("Biancossauro", "Fire", 50, 15);
+	Meowth = cria_pokemon("Meowth", "Grass", 50, 15);
+	lapossauro = cria_pokemon("Lapossauro", "Normal", 200, 100);
+    squirtle = cria_pokemon("Squirtle", "Water", 40, 20);
+    bulbasaur = cria_pokemon("Bulbasaur", "Grass", 70, 10);
+    Charmander = cria_pokemon("Charmander", "Fire", 40, 20);
+    Pidgey = cria_pokemon("Pidgey", "Normal", 50, 15);
+    Rattata = cria_pokemon("Rattata", "Water", 50, 15);
+    Vulpix = cria_pokemon("Vulpix", "Fire", 70, 10);
+    Poliwag = cria_pokemon("Poliwag", "Water", 70, 10);
+	Pikachu = cria_pokemon("Pikachu", "Grass", 40, 20);
 
     del_all();
 
